4c5956e30d265bc2b7b0619ad2345c9b552ca1df
Yujie Liu <125097061+YujieLiu666@users.noreply.github.com>
2025-04-29 15:52:41 -0700

    Update install.R

--- install.R
+++ install.R
@@ -1,19 +1,3 @@
-# install.R
+# Install necessary R packages
+install.packages(c("amerifluxr", "REddyProc", "lubridate", "tidyverse", "dplyr", "bigleaf", "data.table", "ggplot2"))
 
-# Install remotes if not already installed (for GitHub packages)
-if (!requireNamespace("remotes", quietly = TRUE))
-  install.packages("remotes")
-
-# CRAN packages
-install.packages(c(
-  "REddyProc",     # Eddy Covariance data processing
-  "lubridate",     # Date-time handling
-  "tidyverse",     # Data manipulation and visualization
-  "dplyr",         # Data manipulation (also part of tidyverse)
-  "bigleaf",       # Leaf trait estimation
-  "data.table",    # Fast data handling
-  "ggplot2"        # Visualization (also part of tidyverse)
-))
-
-# GitHub packages
-remotes::install_github("AmeriFlux/amerifluxr")
