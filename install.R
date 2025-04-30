f246c816df66fce155dd64367bc1019a3bbd0fe1
Yujie Liu <125097061+YujieLiu666@users.noreply.github.com>
2025-04-29 18:37:11 -0700

    Create install.R
    create

---
+++ install.R
@@ -0,0 +1,10 @@
+# Install necessary R packages
+install.packages(c("amerifluxr", "REddyProc", "lubridate", "tidyverse", "dplyr", "bigleaf", "data.table", "ggplot2"))
+
+# Install devtools if not already installed
+if (!requireNamespace("devtools", quietly = TRUE)) {
+  install.packages("devtools")
+}
+
+# Install amerifluxr from GitHub
+devtools::install_github("chuhousen/amerifluxr")
