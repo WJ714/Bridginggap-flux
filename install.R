de3e796761227d9746e7c7ae4c71b13397670eec
Yujie Liu <125097061+YujieLiu666@users.noreply.github.com>
2025-07-03 15:25:13 -0700

    update
    udpate

--- install.R
+++ install.R
@@ -1,5 +1,6 @@
 # Install necessary R packages
-install.packages(c("amerifluxr", "REddyProc", "lubridate", "tidyverse", "dplyr", "bigleaf", "data.table", "ggplot2"))
+install.packages(c("amerifluxr", "REddyProc", "lubridate", "tidyverse", "dplyr", 
+                   "bigleaf", "data.table", "ggplot2", "ggpubr", "Kendall", "googledrive"))
 
 # Install devtools if not already installed
 if (!requireNamespace("devtools", quietly = TRUE)) {
